44c84fe66ab7e0c94fc7a8eaa5a84d2b437975be
LuigiRac <lui.rac06@gmail.com>
2024-12-02 14:48:49 +0100

    create _array_posts

--- server.js
+++ server.js
@@ -22,3 +22,35 @@ app.listen(port, () => {
 
 })
 
+const posts = [
+    {
+        titolo: "Ciambellone",
+        contenuto: "Ciambellone",
+        immagine: "/img/ciambellone.jpeg",
+        tags: ["Dolce", "Ricetta"]
+    },
+    {
+        titolo: "Cracker Barbabietola",
+        contenuto: "Cracker Barbabietola",
+        immagine: "/img/cracker_barbabietola.jpeg",
+        tags: ["Cracker", "Ricetta", "Barbabietola"]
+    },
+    {
+        titolo: "Pane Fritto Dolce",
+        contenuto: "Pane Fritto Dolce",
+        immagine: "/img/pane_fritto_dolce.jpeg",
+        tags: ["Pane", "Ricetta", "Dolce"]
+    },
+    {
+        titolo: "Pasta Barbabietola",
+        contenuto: "Pasta Barbabietola",
+        immagine: "/img/pasta_barbabietola.jpeg",
+        tags: ["Pasta", "Ricetta", "Barbabietola"]
+    },
+    {
+        titolo: "Torata Paesana",
+        contenuto: "Torta Paesana",
+        immagine: "/img/torta_paesana.jpeg",
+        tags: ["Torta", "Ricetta", "Dolce"]
+    }
+];
